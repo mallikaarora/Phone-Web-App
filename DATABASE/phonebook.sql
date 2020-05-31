b8eb2b0b78a177484326cf389614bed7cafd9f2d
mallikaarora <44220894+mallikaarora@users.noreply.github.com>
2020-05-31 06:32:11 -0700

    update phonebook.sql

--- DATABASE/phonebook.sql
+++ DATABASE/phonebook.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 19, 2019 at 01:23 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
